ac379a21a9cb3b5b85bde54a81f9d3538f38c5de
Michael Zhestkov <michaelzhestkov@gmail.com>
2016-04-04 17:24:31 -0700

    Update RequestInput.java

--- src/main/java/com/sqa/mz/util/helpers/RequestInput.java
+++ src/main/java/com/sqa/mz/util/helpers/RequestInput.java
@@ -16,6 +16,33 @@ public class RequestInput {
 
 	private static Scanner scanner;
 
+	public static boolean getBoolean(String question) {
+		// Local variable to hold input
+		String input;
+		// Infinit loop to look out for Yes and No valid options
+		while (true) {
+			// Ask the user a question to get relative input
+			System.out.print(question + " (Yes/Y) or (No/N):");
+			// Set the input based on what the user enters on their keyboard
+			input = scanner.nextLine();
+			// Check if the user has entered Yes
+			if (input.equalsIgnoreCase("Yes") || input.equalsIgnoreCase("Y")) {
+				// Return that the user has selected Yes so return true
+				return true;
+				// Check if the user has entered No
+			} else if (input.equalsIgnoreCase("No") || input.equalsIgnoreCase("N")) {
+				// Return that the user has selected No so return false
+				return false;
+				// Or else the user has not entered a valid option
+			} else {
+				// Return an error message to user
+				System.out.println("You have not entered a valid option, please choose Yes/Y or No/N.");
+				// Continue infinite loop to ask for a valid response
+				continue;
+			}
+		}
+	}
+
 	// Get a char value from the user
 	public static char getChar(String question) {
 		String input;
@@ -99,6 +126,30 @@ public class RequestInput {
 
 	}
 
+	public static int getInt(String question) {
+		// Local variable to hold temporary number
+		int num;
+		//
+		while (true) {
+			try {
+				// Ask the user a question to get relative input
+				System.out.print(question);
+				// Set the number based on what the user enters on their
+				// keyboard
+				num = Integer.parseInt(scanner.nextLine());
+				// Break out of the infinite loop
+				break;
+			} catch (NumberFormatException e) {
+				// Output to the user that the number is not valid
+				System.out.println("You have not entered a valid number.");
+				// Continue the infinite loop to get a valid number conversion
+				continue;
+			}
+		}
+		// Return number of pets user has entered
+		return num;
+	}
+
 	// Get a int from the user, must be in acceptable range value
 	public static int getInt(String question, int... acceptableNumber) {
 		String input = null;
@@ -125,6 +176,45 @@ public class RequestInput {
 				System.out.println("You have not provided a valid integer type (" + input + ")");
 			} catch (InvalidAcceptableNumberException e) {
 				System.out.println("You have not provided an acceptable valid number (" + input + ")");
+			}
+		}
+	}
+
+	/**
+	 * @param string
+	 * @param string2
+	 * @param i
+	 * @param j
+	 * @param k
+	 * @param l
+	 * @param m
+	 * @return
+	 */
+	public static int getInt(String question, String unacceptableErrorMessage, int min, int max) {
+		String input = null;
+		int resultValue;
+		boolean validInt = false;
+		scanner = new Scanner(System.in);
+		while (true) {
+			try {
+				System.out.print(question);
+				input = scanner.nextLine();
+				resultValue = Integer.parseInt(input);
+
+				if (resultValue >= min && resultValue <= max) {
+					validInt = true;
+				}
+
+				if (!validInt) {
+					throw new InvalidAcceptableNumberException();
+				}
+				return resultValue;
+
+			} catch (NumberFormatException e) {
+				System.out.println("You have not provided a valid integer type (" + input + ")");
+			} catch (InvalidAcceptableNumberException e) {
+				String message = String.format(unacceptableErrorMessage, min, max);
+				System.out.println(message);
 			}
 		}
 	}
@@ -215,6 +305,54 @@ public class RequestInput {
 				return resultValue;
 			} catch (InvalidAcceptableWordException e) {
 				System.out.println("you have not provided valid word " + input);
+			}
+		}
+	}
+
+	public static String getString(String question, String cancellationMessage, Enum... acceptableEnum) {
+		String input = null;
+		String resultValue;
+		String resultString = "";
+		boolean validWord = false;
+		while (true) {
+			try {
+				System.out.print(question);
+				input = scanner.nextLine();
+				resultValue = input;
+
+				for (int i = 0; i < acceptableEnum.length; i++) {
+					if (resultValue.trim().equalsIgnoreCase(acceptableEnum[i].toString())) {
+						validWord = true;
+						resultString += acceptableEnum[i].toString();
+					}
+				}
+				if (!validWord) {
+					throw new InvalidAcceptableWordException();
+				}
+				return resultString;
+			} catch (InvalidAcceptableWordException e) {
+				System.out.println("You have not provided an acceptable word (" + input + ")");
+				System.out.print("You must provide one of the following options: ");
+				for (int i = 0; i < acceptableEnum.length; i++) {
+					System.out.print(acceptableEnum[i].toString());
+					if (i != acceptableEnum.length - 1) {
+						System.out.print(", ");
+					}
+				}
+				System.out.println();
+
+				String message = String.format(cancellationMessage, input);
+				System.out.println(message);
+				// Or can be done in one line
+				// System.out.println(String.format(cancellationMessage,
+				// input));
+
+				System.out.println("If you would like to cancel, enter '*' or just press enter to continue:");
+				input = scanner.nextLine();
+				if (input.equalsIgnoreCase("*")) {
+					return resultString;
+				}
+
 			}
 		}
 	}
